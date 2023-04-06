52ce2383a4c252b396401fab9f7c0b676c4845aa
Ratchasri Kidking <orc_hero@hotmail.com>
2023-04-07 00:54:23 +0700

    add ham menu

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import Container from '../Container'
 import Logo from '../navbar/Logo'
 import Search from '../navbar/Search'
+import UserMenu from '../navbar/UserMenu'
+
 
 
 
@@ -10,13 +12,12 @@ const Navbar = () => {
     <nav className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
 
-
         <Container>
           <div className='flex flex-row justify-between gap-3 md:gap-0'>
             <Logo/>
             <Search/>
+            <UserMenu/>
           </div>
-          
         </Container>
       </div>
     </nav>
