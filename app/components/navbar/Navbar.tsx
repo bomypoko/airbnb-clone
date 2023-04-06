a678218373204fbcfabae90b6436fa09fa4ce4b0
Ratchasri Kidking <orc_hero@hotmail.com>
2023-04-07 00:14:01 +0700

    first Commit

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import Container from '../Container'
+import Logo from '../navbar/Logo'
+
+
+const Navbar = () => {
+  return (
+    <nav className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+
+
+        <Container>
+          <div className='flex flex-row justify-between gap-3 md:gap-0'>
+            <Logo/>
+          </div>
+        </Container>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
+
