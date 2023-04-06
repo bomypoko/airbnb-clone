3dd07aaf7c2b097540d9b22c33809d81236754ae
Ratchasri Kidking <orc_hero@hotmail.com>
2023-04-07 00:34:08 +0700

    Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import Container from '../Container'
 import Logo from '../navbar/Logo'
+import Search from '../navbar/Search'
+
 
 
 const Navbar = () => {
   return (
+
     <nav className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
 
@@ -11,7 +14,9 @@ const Navbar = () => {
         <Container>
           <div className='flex flex-row justify-between gap-3 md:gap-0'>
             <Logo/>
+            <Search/>
           </div>
+          
         </Container>
       </div>
     </nav>
